138613325941a6ab3dd239a3ba4070fc0208ad9e
Malik8008 <tu60prbs1@code.edu.az>
2022-03-04 02:25:23 +0400

    task2

---
+++ My second code/My second code/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace My_second_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int number = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Cavab:");
+            if (number > number2)
+                Console.WriteLine(number);
+            else if (number < number2)
+                Console.WriteLine(number2);
+            else
+                Console.WriteLine("Beraberdir");
+
+
+
+        }
+    }
+}
