753a8c07efb46c94a78c465a01745766b27d2203
Malik8008 <tu60prbs1@code.edu.az>
2022-03-04 02:24:48 +0400

    task1

---
+++ My first code/My first code/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace My_first_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 1)
+                Console.WriteLine("eded tekdir");
+            else
+                Console.WriteLine("eded cutdur");
+        }
+    }
+}
